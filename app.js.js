e8a08af5db4713fcf1b1384860a1b7adc36b15df
Julius Alvarado <angularjs.uiux@gmail.com>
2017-08-04 16:40:10 -0700

    building a prelimary ui

---
+++ app.js.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Julius Alvarado on 8/4/2017.
+ */
---
+++ app/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Julius Alvarado on 8/4/2017.
+ */
+(function () {
+    "use strict";
+    angular.module('MyApp', ['ngMaterial'])
+        .run(function () {
+            console.log("zApp is ready \\^_^/");
+        });
+}());
